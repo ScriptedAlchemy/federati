3d14e9075397311d49930e9f35a0beb848b9b3d4
zackjackson <zackary.l.jackson@gmail.com>
2020-03-02 19:59:22 -0800

    chore: wipe repo in prep for federati monorepo

--- manual/Website1/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import 'webpack-external-import/polyfill';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
-
-ReactDOM.render(<App />, document.getElementById('app'));
--- manual/Website1/webpack.config.js
+++
@@ -1,5 +0,0 @@
-const configFactory = require('../webpack/webpackConfigFactory');
-
-const siteId = 1;
-
-module.exports = configFactory(siteId);
